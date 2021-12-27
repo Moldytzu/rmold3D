500eb2b889440bdb54ad5673875639010a1e4139
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-12-27 15:42:38 +0200

    randomly coloring the triangle

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #include <rmold3D/mold.h>
 
 uint VBO;
+uint IBO;
 uint Shader;
 uint TranslationMatrixLocation;
 
@@ -12,27 +13,24 @@ void render()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear color and depth
 
-    //creating translation matrix
-    Matrix4 translation(
-        1.0f, 0.0f, 0.0f, Scale * 2,
-        0.0f, 1.0f, 0.0f, Scale,
-        0.0f, 0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 0.0f, 1.0f        
-    );
-
-    //telling opengl about the translation matrix
-    glUniformMatrix4fv(TranslationMatrixLocation, 1, GL_TRUE, &translation.i[0][0]);
-
-    //bind vbo
+    //bind vbo & ibo
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+    //position
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,0); //define what type is our data
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6 * sizeof(float),0); //define what type is our data
+
+    //color
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6 * sizeof(float), (void*)(3*sizeof(float))); //define what type is our data
 
     //draw vbo
-    glDrawArrays(GL_TRIANGLES,0,3);
+    glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_INT,0);
 
     //disable vbo
     glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
 
     glutPostRedisplay(); //call again redraw
     glutSwapBuffers(); //do double buffering
@@ -73,35 +71,51 @@ int main(int argc, char** argv)
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
 
+    //init rand
+    srand(time(NULL));
+
     //triangle
-    Vector3 vertices[3];
-    vertices[0] = Vector3(0,1,0); //up
-    vertices[1] = Vector3(-1,-1,0); //down left
-    vertices[2] = Vector3(1,-1,0); //down right
+    Vertex vertices[3];
+    vertices[0] = Vertex(0,1,0); //up
+    vertices[1] = Vertex(-1,-1,0); //down left
+    vertices[2] = Vertex(1,-1,0); //down right
 
     //vbo for triangle
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    //ibo for triangle
+    uint Indices[] = {
+        0,1,2,
+        3,4,5,
+        6,7,8,
+    };
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+
     //define shaders
     const char* vertexsrc =
     "#version 330 core\n" \
     "layout (location = 0) in vec3 Position;\n" \
-    "uniform mat4 translationMatrix;\n" \
+    "layout (location = 1) in vec3 inColor;\n" \
+    "out vec4 Color;\n" \
     "void main()\n" \
     "{\n" \
-    " gl_Position = translationMatrix * vec4(Position, 1.0);\n" \
+    " gl_Position = vec4(Position, 1.0);\n" \
+    " Color = vec4(inColor, 1.0);\n" \
     "}\n" \
     "\n"; //gl position is the dot product of the translation matrix and the position itself
 
     const char* fragmentsrc =
     "#version 330 core\n" \
     "out vec4 FragColor;\n" \
+    "in vec4 Color;\n" \
     " \n" \
     "void main()\n" \
     "{\n" \
-    " FragColor = vec4(1.0, 1.0, 0.0, 0.0);\n" \
+    " FragColor = Color;\n" \
     "}\n" \
     "\n";
 
--- rmold3D/mold.h
+++ rmold3D/mold.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <cstring>
 #include <map>
+#include <time.h>
 
 class Vector3
 {
@@ -35,5 +36,26 @@ public:
         i[1][0] = v10; i[1][1] = v11; i[1][2] = v12; i[1][3] = v13;
         i[2][0] = v20; i[2][1] = v21; i[2][2] = v22; i[2][3] = v23;
         i[3][0] = v30; i[3][1] = v31; i[3][2] = v32; i[3][3] = v33;
+    }
+};
+
+static float RandomFloat(float a, float b) {
+    float random = ((float) rand()) / (float) RAND_MAX;
+    float diff = b - a;
+    float r = random * diff;
+    return a + r;
+}
+
+class Vertex
+{
+public:
+    Vector3 position;
+    Vector3 color;
+
+    Vertex() {}
+    Vertex(float x,float y,float z)
+    {
+        position = Vector3(x,y,z);
+        color = Vector3(RandomFloat(0,1),RandomFloat(0,1),RandomFloat(0,1));
     }
 };
