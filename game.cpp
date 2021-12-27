16c941564223f0edd8a224b72c944303a625354c
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-12-27 14:25:27 +0200

    drawing a point using the rasterizer

--- game.cpp
+++ game.cpp
@@ -1,9 +1,38 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
+class Vector3
+{
+public:
+    float X;
+    float Y;
+    float Z;
+
+    Vector3() {}
+    Vector3(float x,float y,float z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+};
+
+unsigned int VBO;
+
 void render()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear color and depth
+
+    //bind vbo
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,0); //define what type is our data
+
+    //draw vbo
+    glDrawArrays(GL_POINTS,0,1);
+
+    //disable vbo
+    glDisableVertexAttribArray(0);
 
     glutPostRedisplay(); //call again redraw
     glutSwapBuffers(); //do double buffering
@@ -16,13 +45,18 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE); //rgba, double buffering, depth
     glutInitWindowSize(800,600); //800x600
     glutCreateWindow("rmold3D"); //create window
+    glutDisplayFunc(render); //on render
 
     //init glew
     glewInit();
-
     glClearColor(0,0,0,0); // black
 
-    glutDisplayFunc(render); //on render
+    //vbo
+    Vector3 vertices[1];
+    vertices[0] = Vector3(0,0,0);
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glutMainLoop(); //run app
 
