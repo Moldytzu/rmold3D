ae853cc23e8463dac0d49ea6e524d3c784fb4908
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2021-12-28 15:39:58 +0200

    moved texture into mold.h

--- game.cpp
+++ game.cpp
@@ -17,46 +17,10 @@ float lastFrame = 0.0f;
 
 unsigned int VBO;
 unsigned int VAO;
-unsigned int EBO;
 unsigned int shaderProgram;
 unsigned int texture;
 
-struct Texture
-{
-    uint32_t Size;
-    uint32_t Width;
-    uint32_t Height;
-
-    uint8_t *PixelData;
-};
-
-struct BitmapImageHeader //https://en.wikipedia.org/wiki/BMP_file_format
-{
-    //bmp header
-    uint8_t Signature1; //Should be B
-    uint8_t Signature2; //Should be M
-
-    uint32_t Size; //bmp file size
-
-    uint32_t Reserved;
-
-    uint32_t DataOffset; //offset to the image data
-
-    //BITMAPINFOHEADER
-    uint32_t HeaderSize;          //size of BITMAPINFOHEADER, always 0x28 (decimal 40)
-    int32_t BitmapWidth;          //width of the image
-    int32_t BitmapHeight;         //height of the image
-    uint16_t Planes;              //bit planes
-    uint16_t BPP;                 //bits per pixel
-    uint32_t CompressionMethod;   //the method of compression used
-    uint32_t ImageSize;           //size in bytes of the whole image, without headers
-    int32_t HorizontalResolution; //resolution in pixel per metre
-    int32_t VerticalResolution;   //resolution in pixel per metre
-    uint32_t Colors;              //colors in the color palette
-    uint32_t ImportantColors;     //important colors used, generally ignored
-} __attribute__((packed));
-
-Texture LoadRGBBitmap(const char *filename)
+mold::render::image::Texture LoadRGBBitmap(const char *filename)
 {
     FILE *file;
     uint32_t size;
@@ -75,14 +39,14 @@ Texture LoadRGBBitmap(const char *filename)
         return {};
     }
 
-    BitmapImageHeader *header = (BitmapImageHeader *)malloc(sizeof(BitmapImageHeader));
-    if (fread(header, sizeof(BitmapImageHeader), 1, file) != 1)
+    mold::render::image::BitmapImageHeader *header = (mold::render::image::BitmapImageHeader *)malloc(sizeof(mold::render::image::BitmapImageHeader));
+    if (fread(header, sizeof(mold::render::image::BitmapImageHeader), 1, file) != 1)
         return {};
 
     if (header->BPP != 24)
         return {};
 
-    Texture texture;
+    mold::render::image::Texture texture;
 
     texture.Width = header->BitmapWidth;
     texture.Height = header->BitmapHeight;
@@ -308,7 +272,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    Texture t = LoadRGBBitmap("texture.bmp");
+    mold::render::image::Texture t = LoadRGBBitmap("texture.bmp");
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, t.Width, t.Height, 0, GL_RGB, GL_UNSIGNED_BYTE, t.PixelData);
     glGenerateMipmap(GL_TEXTURE_2D);
 
--- rmold3D/mold.h
+++ rmold3D/mold.h
@@ -5,3 +5,47 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+namespace mold
+{
+    namespace render
+    {
+        namespace image
+        {
+            struct Texture
+            {
+                uint32_t Size;
+                uint32_t Width;
+                uint32_t Height;
+
+                uint8_t *PixelData;
+            };
+
+            struct BitmapImageHeader //https://en.wikipedia.org/wiki/BMP_file_format
+            {
+                //bmp header
+                uint8_t Signature1; //Should be B
+                uint8_t Signature2; //Should be M
+
+                uint32_t Size; //bmp file size
+
+                uint32_t Reserved;
+
+                uint32_t DataOffset; //offset to the image data
+
+                //BITMAPINFOHEADER
+                uint32_t HeaderSize;          //size of BITMAPINFOHEADER, always 0x28 (decimal 40)
+                int32_t BitmapWidth;          //width of the image
+                int32_t BitmapHeight;         //height of the image
+                uint16_t Planes;              //bit planes
+                uint16_t BPP;                 //bits per pixel
+                uint32_t CompressionMethod;   //the method of compression used
+                uint32_t ImageSize;           //size in bytes of the whole image, without headers
+                int32_t HorizontalResolution; //resolution in pixel per metre
+                int32_t VerticalResolution;   //resolution in pixel per metre
+                uint32_t Colors;              //colors in the color palette
+                uint32_t ImportantColors;     //important colors used, generally ignored
+            } __attribute__((packed));
+        };
+    };
+};
