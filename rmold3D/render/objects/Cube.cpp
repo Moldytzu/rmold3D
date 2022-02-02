f4a540131c90e40088f42978635a14e548e21003
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2022-02-02 21:36:46 +0200

    fix cube texture coords

--- rmold3D/render/objects/Cube.cpp
+++ rmold3D/render/objects/Cube.cpp
@@ -6,42 +6,48 @@ mold::render::objects::Cube::Cube(mold::render::image::Texture texture) { Init(t
 void mold::render::objects::Cube::Init(mold::render::image::Texture texture)
 {
     float vertices[] = {
-        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 0.0f), // back
-        Vertex(0.5f, -0.5f, -0.5f), TexCoord(1.0f, 0.0f),
-        Vertex(0.5f, 0.5f, -0.5f), TexCoord(1.0f, 1.0f),
-        Vertex(0.5f, 0.5f, -0.5f), TexCoord(1.0f, 1.0f),
+        // back face
+        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 0.0f), // right up
+        Vertex(0.5f, -0.5f, -0.5f), TexCoord(1.0f, 0.0f),  // left up
+        Vertex(0.5f, 0.5f, -0.5f), TexCoord(1.0f, 1.0f),   // left down
+        Vertex(0.5f, 0.5f, -0.5f), TexCoord(1.0f, 1.0f),   // left down
+        Vertex(-0.5f, 0.5f, -0.5f), TexCoord(0.0f, 1.0f),  // right down
+        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 0.0f), // right up
+
+        // front face
+        Vertex(-0.5f, -0.5f, 0.5f), TexCoord(0.0f, 0.0f), // right up
+        Vertex(0.5f, -0.5f, 0.5f), TexCoord(1.0f, 0.0f),  // left up
+        Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 1.0f),   // left down
+        Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 1.0f),   // left down
+        Vertex(-0.5f, 0.5f, 0.5f), TexCoord(0.0f, 1.0f),  // right down
+        Vertex(-0.5f, -0.5f, 0.5f), TexCoord(0.0f, 0.0f), // right up
+
+        // left face
+        Vertex(-0.5f, 0.5f, 0.5f), TexCoord(1.0f, 1.0f),   // right up
+        Vertex(-0.5f, 0.5f, -0.5f), TexCoord(0.0f, 1.0f),  // left up
+        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 0.0f), // left down
+        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 0.0f), // left down
+        Vertex(-0.5f, -0.5f, 0.5f), TexCoord(1.0f, 0.0f),  // right down
+        Vertex(-0.5f, 0.5f, 0.5f), TexCoord(1.0f, 1.0f),   // right up
+
+        // right face
+        Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 1.0f),   // right up
+        Vertex(0.5f, 0.5f, -0.5f), TexCoord(0.0f, 1.0f),  // left up
+        Vertex(0.5f, -0.5f, -0.5f), TexCoord(0.0f, 0.0f), // left down
+        Vertex(0.5f, -0.5f, -0.5f), TexCoord(0.0f, 0.0f), // left down
+        Vertex(0.5f, -0.5f, 0.5f), TexCoord(1.0f, 0.0f),  // right down
+        Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 1.0f),   // right up
+
+        // bottom face
+        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 1.0f), // right up
+        Vertex(0.5f, -0.5f, -0.5f), TexCoord(1.0f, 1.0f),  // left up
+        Vertex(0.5f, -0.5f, 0.5f), TexCoord(1.0f, 0.0f),   // left down
+        Vertex(0.5f, -0.5f, 0.5f), TexCoord(1.0f, 0.0f),   // left down
+        Vertex(-0.5f, -0.5f, 0.5f), TexCoord(0.0f, 0.0f),  // right down
+        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 1.0f), // right up
+
+        // up
         Vertex(-0.5f, 0.5f, -0.5f), TexCoord(0.0f, 1.0f),
-        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 0.0f),
-
-        Vertex(-0.5f, -0.5f, 0.5f), TexCoord(0.0f, 0.0f), // front
-        Vertex(0.5f, -0.5f, 0.5f), TexCoord(1.0f, 0.0f),
-        Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 1.0f),
-        Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 1.0f),
-        Vertex(-0.5f, 0.5f, 0.5f), TexCoord(0.0f, 1.0f),
-        Vertex(-0.5f, -0.5f, 0.5f), TexCoord(0.0f, 0.0f),
-
-        Vertex(-0.5f, 0.5f, 0.5f), TexCoord(1.0f, 0.0f), // left
-        Vertex(-0.5f, 0.5f, -0.5f), TexCoord(1.0f, 1.0f),
-        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 1.0f),
-        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 1.0f),
-        Vertex(-0.5f, -0.5f, 0.5f), TexCoord(0.0f, 0.0f),
-        Vertex(-0.5f, 0.5f, 0.5f), TexCoord(1.0f, 0.0f),
-
-        Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 0.0f), // right
-        Vertex(0.5f, 0.5f, -0.5f), TexCoord(1.0f, 1.0f),
-        Vertex(0.5f, -0.5f, -0.5f), TexCoord(0.0f, 1.0f),
-        Vertex(0.5f, -0.5f, -0.5f), TexCoord(0.0f, 1.0f),
-        Vertex(0.5f, -0.5f, 0.5f), TexCoord(0.0f, 0.0f),
-        Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 0.0f),
-
-        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 1.0f), // bottom
-        Vertex(0.5f, -0.5f, -0.5f), TexCoord(1.0f, 1.0f),
-        Vertex(0.5f, -0.5f, 0.5f), TexCoord(1.0f, 0.0f),
-        Vertex(0.5f, -0.5f, 0.5f), TexCoord(1.0f, 0.0f),
-        Vertex(-0.5f, -0.5f, 0.5f), TexCoord(0.0f, 0.0f),
-        Vertex(-0.5f, -0.5f, -0.5f), TexCoord(0.0f, 1.0f),
-
-        Vertex(-0.5f, 0.5f, -0.5f), TexCoord(0.0f, 1.0f), // up
         Vertex(0.5f, 0.5f, -0.5f), TexCoord(1.0f, 1.0f),
         Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 0.0f),
         Vertex(0.5f, 0.5f, 0.5f), TexCoord(1.0f, 0.0f),
@@ -51,17 +57,17 @@ void mold::render::objects::Cube::Init(mold::render::image::Texture texture)
     if (Initialized) // if initialized just retun
         return;
 
-    Initialized = Enabled = true; // enable and initialize
-    Texture = texture; // set texture
+    Initialized = Enabled = true;                          // enable and initialize
+    Texture = texture;                                     // set texture
     Vabo = mold::render::VABO(vertices, sizeof(vertices)); // generate VBO & VAO
 }
 
 void mold::render::objects::Cube::Draw()
 {
-    Vabo.Bind(); // bind vao & vbo
-    Texture.Bind(); // bind texture
-    mold::render::shader::SetUniform4fv("model",PositionMatrix); //give the shader our position matrix
-    mold::render::DrawTriangles(36); //draw 36 triangles
+    Vabo.Bind();                                                  // bind vao & vbo
+    Texture.Bind();                                               // bind texture
+    mold::render::shader::SetUniform4fv("model", PositionMatrix); //give the shader our position matrix
+    mold::render::DrawTriangles(36);                              //draw 36 triangles
 }
 
 std::string mold::render::objects::Cube::Type()
