9ff10ff1714d2d6a92f2f5ef767741f429bedea0
Alex <40271862+Moldytzu@users.noreply.github.com>
2022-02-04 23:30:23 +0200

    fluid camera movement

--- game.cpp
+++ game.cpp
@@ -11,59 +11,64 @@
 double lastX;
 double lastY;
 
-double mouseSens = 0.005f;
+double mouseSens = 0.05f;
 
-void onTick()
+void doMouse()
 {
-// handle mouse input for camera
-double xpos, ypos;
-glfwGetCursorPos(mold::GlobalWindow,&xpos, &ypos);
+    // handle mouse input for camera
+    double xpos, ypos;
+    glfwGetCursorPos(mold::GlobalWindow, &xpos, &ypos);
 
-if(xpos > mold::settings::WindowWidth)
-{
-    glfwSetCursorPos(mold::GlobalWindow,10,ypos);
-    lastX = 10;
-}
-
-if(xpos < 0)
-{
-    glfwSetCursorPos(mold::GlobalWindow,mold::settings::WindowWidth-10,ypos);
-    lastX = mold::settings::WindowWidth-10;
-}
-
-
-if(ypos > mold::settings::WindowHeight)
-{
-    glfwSetCursorPos(mold::GlobalWindow,xpos,10);
-    lastY = 10;
-}
-
-
-if(ypos < 0)
-{
-    glfwSetCursorPos(mold::GlobalWindow,xpos,mold::settings::WindowHeight-10);
-    lastY = mold::settings::WindowHeight-10;
-}
-
-
-    if(lastX != xpos)
+    if (xpos > mold::settings::WindowWidth)
     {
-        double offsetX = xpos-lastX;
-        
+        glfwSetCursorPos(mold::GlobalWindow, 10, ypos);
+        lastX = 10;
+        return;
+    }
+
+    if (xpos < 0)
+    {
+        glfwSetCursorPos(mold::GlobalWindow, mold::settings::WindowWidth - 10, ypos);
+        lastX = mold::settings::WindowWidth - 10;
+        return;
+    }
+
+    if (ypos > mold::settings::WindowHeight)
+    {
+        glfwSetCursorPos(mold::GlobalWindow, xpos, 10);
+        lastY = 10;
+        return;
+    }
+
+    if (ypos < 0)
+    {
+        glfwSetCursorPos(mold::GlobalWindow, xpos, mold::settings::WindowHeight - 10);
+        lastY = mold::settings::WindowHeight - 10;
+        return;
+    }
+
+    if (lastX != xpos)
+    {
+        double offsetX = xpos - lastX;
+
         mold::render::camera::Yaw += offsetX * mouseSens;
 
         lastX = xpos;
     }
 
-    if(lastY != ypos)
+    if (lastY != ypos)
     {
-        double offsetY = lastY-ypos;
+        double offsetY = lastY - ypos;
 
-        mold::render::camera::Pitch += offsetY * mouseSens ;
+        mold::render::camera::Pitch += offsetY * mouseSens;
 
         lastY = ypos;
     }
+}
 
+void onTick()
+{
+    doMouse();
     const float cameraSpeed = 20.0f * mold::time::DeltaTime; // adjust accordingly
     if (mold::input::GetKey('W'))
         mold::render::camera::Rotate(mold::render::CameraDirection::Forward, cameraSpeed * 2);
@@ -107,19 +112,16 @@ void onExit()
     mold::log::Info("Goodbye!");
 }
 
-
-
 bool first = true;
-void onMouse(GLFWwindow* window, double xpos, double ypos)
+void onMouse(GLFWwindow *window, double xpos, double ypos)
 {
-    if(first)
+    if (first)
     {
         lastX = xpos;
         lastY = ypos;
         first = false;
         return;
     }
-
 }
 
 //entry point
@@ -128,16 +130,16 @@ int main()
     if (!mold::Init(800, 600))
         mold::Destroy();
 
-if (glfwRawMouseMotionSupported())
-    glfwSetInputMode(mold::GlobalWindow, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+    if (glfwRawMouseMotionSupported())
+        glfwSetInputMode(mold::GlobalWindow, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 
-    glfwSetCursorPos(mold::GlobalWindow,400,300);
+    glfwSetCursorPos(mold::GlobalWindow, 400, 300);
 
-    mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture("texture.bmp")),"Simple Cube");
+    mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture("texture.bmp")), "Simple Cube");
 
     mold::GlobalGameObjects.Get("Simple Cube")->Move(glm::vec3(0, 1.0f, -1.0f));
     mold::GlobalGameObjects.Get("Simple Cube")->Translate(glm::vec3(0, 1.0f, -1.0f));
-    mold::GlobalGameObjects.Get("Simple Cube")->Scale(glm::vec3(2.0f,1.0f,1.0f));
+    mold::GlobalGameObjects.Get("Simple Cube")->Scale(glm::vec3(2.0f, 1.0f, 1.0f));
     mold::GlobalGameObjects.Get("Simple Cube")->Opacity = 0.5f;
 
     //Callbacks
@@ -147,15 +149,15 @@ if (glfwRawMouseMotionSupported())
     mold::GlobalEventSystem.AttachCallback(mold::EventType::Exit, onExit);
 
     //Instantiate a cube
-    mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture(mold::render::Colour(255,0,0))));
-    mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture(mold::render::Colour(0,255,0))));
-    mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture(mold::render::Colour(0,0,255))));
+    mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture(mold::render::Colour(255, 0, 0))));
+    mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture(mold::render::Colour(0, 255, 0))));
+    mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture(mold::render::Colour(0, 0, 255))));
     mold::GlobalGameObjects.Instantiate(new mold::render::objects::Cube(mold::render::image::Texture(mold::render::Colour(255))));
     mold::GlobalGameObjects.Instantiate(new mold::render::objects::GameObject());
 
-    mold::GlobalGameObjects.Get("Textured Cube")->Move(glm::vec3(1.0f,1.0f,2.0f));
-    mold::GlobalGameObjects.Get("Textured Cube 1")->Move(glm::vec3(-1.0f,0.5f,-2.0f));
-    mold::GlobalGameObjects.Get("Textured Cube 2")->Move(glm::vec3(1.0f,0.0f,2.0f));
+    mold::GlobalGameObjects.Get("Textured Cube")->Move(glm::vec3(1.0f, 1.0f, 2.0f));
+    mold::GlobalGameObjects.Get("Textured Cube 1")->Move(glm::vec3(-1.0f, 0.5f, -2.0f));
+    mold::GlobalGameObjects.Get("Textured Cube 2")->Move(glm::vec3(1.0f, 0.0f, 2.0f));
 
     //main loop
     mold::Run();
