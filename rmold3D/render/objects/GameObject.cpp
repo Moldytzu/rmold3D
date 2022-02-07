1861b34b212c70062abf4a2b2b6dc9be77127c81
Moldytzu <xmalecsxxrovertebral@yahoo.com>
2022-02-07 21:23:17 +0200

    clean up

--- rmold3D/render/objects/GameObject.cpp
+++ rmold3D/render/objects/GameObject.cpp
@@ -1,60 +1,12 @@
 #include <rmold3D/mold.h>
 
+// Components
+
 std::unordered_map<std::string, mold::render::objects::Component *> Components;
 
 std::unordered_map<std::string, mold::render::objects::Component *> mold::render::objects::GameObject::GetComponents()
 {
     return Components;
-}
-
-mold::render::objects::GameObject::GameObject() {}
-
-void mold::render::objects::GameObject::Draw() {}
-
-mold::render::objects::GameObject::~GameObject()
-{
-    Texture.Deallocate(); // deallocate texture and vabo
-    Vabo.Deallocate();
-}
-
-mold::render::objects::GameObject::GameObject(mold::render::image::Texture texture)
-{
-    Texture = texture; // set texture and enable the object
-    Enabled = true;
-}
-
-void mold::render::objects::GameObject::Translate(glm::vec3 offset)
-{
-    PositionMatrix = glm::translate(PositionMatrix, offset); // translate matrix
-}
-
-void mold::render::objects::GameObject::Move(glm::vec3 position)
-{
-    PositionMatrix = glm::translate(glm::mat4(1.0f), position); // move matrix by translating a new one to the desired position
-}
-
-void mold::render::objects::GameObject::Scale(glm::vec3 scaleFactor)
-{
-    PositionMatrix = glm::scale(PositionMatrix, scaleFactor); // scale matrix
-}
-
-void mold::render::objects::GameObject::Rotate(glm::vec3 axis, float angle)
-{
-    PositionMatrix = glm::rotate(PositionMatrix, glm::radians(angle), glm::normalize(axis)); // rotate matrix
-}
-
-void mold::render::objects::GameObject::ReplaceTexture(mold::render::image::Texture newTexture)
-{
-    Texture.Deallocate(); // deallocate so we don't create memory leaks or smth
-    Texture = newTexture; // replace the old texture
-}
-
-void mold::render::objects::GameObject::Bind()
-{
-    Vabo.Bind();                                                                         // bind vao & vbo
-    Texture.Bind();                                                                      // bind texture
-    mold::render::shader::SetUniform4fv("model", PositionMatrix);                        // give the shader our position matrix
-    mold::render::shader::SetUniform4v("fcolour", glm::vec4(1.0f, 1.0f, 1.0f, Opacity)); // pass colour information needed for transparency
 }
 
 void mold::render::objects::GameObject::AttachComponent(std::string name, Component *component)
@@ -103,12 +55,72 @@ bool mold::render::objects::GameObject::ExistsComponent(std::string name)
     return Components.contains(name);
 }
 
-std::string mold::render::objects::GameObject::Type()
+// Default functions
+
+void mold::render::objects::GameObject::Draw() {}
+
+// Constructors and distructors
+
+mold::render::objects::GameObject::GameObject() {}
+
+mold::render::objects::GameObject::GameObject(mold::render::image::Texture texture)
 {
-    return "Empty"; // empty game object
+    Texture = texture; // set texture and enable the object
+    Enabled = true;
+}
+
+mold::render::objects::GameObject::~GameObject()
+{
+    Texture.Deallocate(); // deallocate texture and vabo
+    Vabo.Deallocate();
+}
+
+// Positioning
+
+void mold::render::objects::GameObject::Translate(glm::vec3 offset)
+{
+    PositionMatrix = glm::translate(PositionMatrix, offset); // translate matrix
+}
+
+void mold::render::objects::GameObject::Move(glm::vec3 position)
+{
+    PositionMatrix = glm::translate(glm::mat4(1.0f), position); // move matrix by translating a new one to the desired position
+}
+
+void mold::render::objects::GameObject::Scale(glm::vec3 scaleFactor)
+{
+    PositionMatrix = glm::scale(PositionMatrix, scaleFactor); // scale matrix
+}
+
+void mold::render::objects::GameObject::Rotate(glm::vec3 axis, float angle)
+{
+    PositionMatrix = glm::rotate(PositionMatrix, glm::radians(angle), glm::normalize(axis)); // rotate matrix
 }
 
 glm::vec3 mold::render::objects::GameObject::GetPosition()
 {
     return PositionMatrix[3]; // position is stored on the 3rd axis (is this the correct term?)
+}
+
+// Texturing
+
+void mold::render::objects::GameObject::ReplaceTexture(mold::render::image::Texture newTexture)
+{
+    Texture.Deallocate(); // deallocate so we don't create memory leaks or smth
+    Texture = newTexture; // replace the old texture
+}
+
+// Run-time
+
+void mold::render::objects::GameObject::Bind()
+{
+    Vabo.Bind();                                                                         // bind vao & vbo
+    Texture.Bind();                                                                      // bind texture
+    mold::render::shader::SetUniform4fv("model", PositionMatrix);                        // give the shader our position matrix
+    mold::render::shader::SetUniform4v("fcolour", glm::vec4(1.0f, 1.0f, 1.0f, Opacity)); // pass colour information needed for transparency
+}
+
+std::string mold::render::objects::GameObject::Type()
+{
+    return "Empty"; // empty game object
 }
